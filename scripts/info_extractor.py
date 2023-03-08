74d2a13bf509c4de5327aeda2e4896a24271729b
nickpoz97 <nega9705@gmail.com>
2023-03-08 14:55:26 +0100

    updated info extractor

--- scripts/info_extractor.py
+++ scripts/info_extractor.py
@@ -2,14 +2,14 @@ import subprocess
 import os
 import json
 
-def execute_instance(exe_path: str, agents_file_path: str, tasks_file_path: str, nAgents: int, nTasks: int):
+def execute_instance(exe_path: str, agents_file_path: str, tasks_file_path: str):
     command = [exe_path, '--a', agents_file_path, '--t', tasks_file_path]
     #command = f"{exe_path} --a {agents_file_path} --t {tasks_file_path}"
 
     result = subprocess.run(command, capture_output=True, check=True)
     return result.stdout.decode("UTF-8")
 
-def extract_dict(decoded_output: str):
+def instance_stats(decoded_output: str):
     lines = decoded_output.strip().split('\n')
     header_line_n = 3
 
@@ -24,6 +24,13 @@ def extract_dict(decoded_output: str):
     ]
     return infos
 
+def all_stats(exe_path: str, instances_root: str):
+    tasks_files = [os.path.join(instances_root, name) for name in filter(lambda fname: os.path.splitext(fname)[-1] == '.tasks', os.listdir(instances_root))]
+    agents_files = [os.path.join(instances_root, name) for name in filter(lambda fname: os.path.splitext(fname)[-1] == '.agents', os.listdir(instances_root))]
+    
+    return [instance_stats(execute_instance(exe_path, af, tf)) for af, tf in zip(agents_files, tasks_files)]
+
 if __name__ == "__main__":
-    res = execute_instance("./out/evaluation", os.path.normpath("./a40_t130/0.agents"), os.path.normpath("./a40_t130/0.tasks"), 40, 130)
-    print(extract_dict(res))
+    # res = execute_instance("./out/evaluation", os.path.normpath("./a40_t130/0.agents"), os.path.normpath("./a40_t130/0.tasks"), 40, 130)
+    # print(instance_stats(res))
+    print(all_stats("out/evaluation", "a40_t130"))
